9e680f0583b96c65548c2e22ed86cb16da5b44a0
zgy441008825 <441008825@qq.com>
2020-04-01 16:51:27 +0800

    优化文件帮助类   搜索方法

--- app/src/main/java/com/zougy/zio/MainActivity.kt
+++ app/src/main/java/com/zougy/zio/MainActivity.kt
@@ -1,15 +1,46 @@
 package com.zougy.zio
 
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.zougy.commons.ZLog
+import com.zougy.ziolib.files.FileTypeEnum
+import com.zougy.ziolib.files.ZFileTools
+import com.zougy.ziolib.files.ZFileTools.SearchFileCallback
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
-
-    private var beanList = mutableListOf<TideDemoBean>()
-    private val adapter = MainViewAdapter(beanList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadingData()
+    }
+
+    private fun loadingData() {
+        Flowable.just(Environment.getExternalStorageDirectory())
+            .subscribeOn(Schedulers.newThread())
+            .map { ZFileTools.searchFile(it, FileTypeEnum.FILE_TYPE_MP3, null, true, showHidden = false, callback = searchMp3Callback) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                ZLog.d(it?.size)
+            }, {
+                it.printStackTrace()
+            })
+    }
+
+    private val searchMp3Callback = object : SearchFileCallback {
+        override fun onFind(file: File) {
+            ZLog.d(file)
+        }
+
+        override fun onInDir(dir: File) {
+            ZLog.d(dir)
+        }
     }
 }
